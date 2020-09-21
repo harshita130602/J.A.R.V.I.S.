34ed05c9220811642c887484fd08756a0f597eae
Harshita Upadhyay <48062137+harshita130602@users.noreply.github.com>
2020-09-22 02:26:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def assistant(command):
         reg_ex = re.search('open website (.+)', command)
         if reg_ex:
             domain = reg_ex.group(1)
-            url = 'https://www.' + domain
+            url = 'https://www.' + domain + '.com'
             webbrowser.open(url)
             print('Done!')
         else:
